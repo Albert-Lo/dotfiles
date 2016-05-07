b1b9d972026288104b7d85b0698ce5f5833a8033
Albert <sero.albert@gmail.com>
2016-05-06 17:14:04 -0700

    Delete ssh aliases from git

--- zsh/ssh.zsh
+++
@@ -1,7 +0,0 @@
-alias vneck='ssh albert@vneck-3.ooapi.com -L 27018:localhost:27017'
-alias gloves='ssh albert@gloves.ooapi.com'
-alias hoodie='ssh albert@hoodie-0.ooapi.com'
-alias hoodie1='ssh albert@hoodie-1.ooapi.com'
-alias tanktop='ssh albert@tanktop-0.ooapi.com -L 27019:localhost:27017'
-alias pajamas='ssh albert@pajamas.ooapi.com -L 27020:localhost:27017'
-alias partyhut='ssh albert@api.partyhut.hk -L 28017:localhost:27017'
