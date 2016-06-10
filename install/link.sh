dab05806f298e881acd2d897dd98888b3a706139
Albert <sero.albert@gmail.com>
2016-06-10 13:10:07 -0700

    Fix symlink script

--- install/link.sh
+++ install/link.sh
@@ -4,8 +4,7 @@ DOTFILES=$HOME/dotfiles
 
 echo -e "\nCreating symlinks"
 echo "=============================="
-linkables=$( find -H "$DOTFILES" -maxdepth 4 -name '*.symlink' )
-for file in $linkables ; do
+for file in $( find $1 -H "$DOTFILES" -maxdepth 4 -name '*.symlink' ) ; do
     target="$HOME/.$( basename $file ".symlink" )"
     if [ -e $target ]; then
         echo "~${target#$HOME} already exists... Skipping."
@@ -21,7 +20,7 @@ if [ ! -d $HOME/.config ]; then
     echo "Creating ~/.config"
     mkdir -p $HOME/.config
 fi
-# configs=$( find -path "$DOTFILES/config.symlink" -maxdepth 1 )
+
 for config in $DOTFILES/config/*; do
     target=$HOME/.config/$( basename $config )
     if [ -e $target ]; then
